a27984f91e8f84a7d684ce0b907b413b3f57e8b3
princewilling <princewillingoo@gmail.com>
2023-03-24 11:52:18 +0100

    variables & constant

---
+++ variables.js
@@ -0,0 +1,35 @@
+// let message;
+
+// message = 'Hello'; // store the string 'Hello' in the variable named message
+
+// console.log(message)
+// //alert(message); // shows the variable content
+
+let message = 'Hello!'; // define the variable and assign the value
+
+//alert(message); // Hello!
+
+let userName;
+let test123;
+
+let $ = 1; // declared a variable with the name "$"
+let _ = 2; // and now a variable with the name "_"
+
+// alert($ + _); // 3
+console.log($ + _)
+
+// Constant
+const myBirthday = '18.04.1982';
+
+myBirthday = '01.01.2001'; // error, can't reassign the constant!
+
+// Uppercase constants
+
+const COLOR_RED = "#F00";
+const COLOR_GREEN = "#0F0";
+const COLOR_BLUE = "#00F";
+const COLOR_ORANGE = "#FF7F00";
+
+// ...when we need to pick a color
+let color = COLOR_ORANGE;
+alert(color); // #FF7F00
