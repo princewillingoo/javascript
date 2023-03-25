49925f5597af5a9fc55a40879246dc69a3f6cdfe
princewilling <princewillingoo@gmail.com>
2023-03-25 20:58:33 +0100

    interactions

---
+++ interactions.js
@@ -0,0 +1,15 @@
+alert("Hello");
+
+// The word “modal” means that the visitor can’t interact with the rest of the page, 
+// press other buttons, etc, until they have dealt with the window.
+
+let age = prompt('How old are you?', 100);
+
+alert(`You are ${age} years old!`); // You are 100 years old!
+
+result = confirm(question);
+
+// The function confirm shows a modal window with a question and two buttons: OK and Cancel.
+let isBoss = confirm("Are you the boss?");
+
+alert( isBoss ); // true if OK is pressed
