ce60633e915407b2685fc0f68f4ae4c646bf3e5e
princewilling <princewillingoo@gmail.com>
2023-05-21 17:35:05 +0100

    resolve errors in arrow-func.js

--- arrow-func.js
+++ arrow-func.js
@@ -1,20 +1,21 @@
 // Arrow functions, the basics
 
-// There’s another very simple and concise syntax for creating functions, that’s often better than Function Expressions.
+// There’s another very simple and concise syntax for creating functions, 
+// that’s often better than Function Expressions.
 
 // It’s called “arrow functions”, because it looks like this:
 
-let func_ = (arg1, arg2, ..., argN) => expression;
+/*let func_ = (arg1, arg2, ..., argN) => expression;*/
 
 // In other words, it’s the shorter version of:
 
-let func = function(arg1, arg2, ..., argN) {
+let func = function(arg1, arg2, ... argN) {
   return expression;
 };
 
 // Let’s see a concrete example:
 
-let sum = (a, b) => a + b;
+let sumx = (a, b) => a + b;
 
 /* This arrow function is a shorter form of:
 
@@ -23,7 +24,7 @@ let sum = function(a, b) {
 };
 */
 
-alert( sum(1, 2) ); // 3
+alert( sumx(1, 2) ); // 3
 
 // As you can see, (a, b) => a + b means a function that accepts two arguments named a and b. Upon the execution, it evaluates the expression a + b and returns the result.
 
@@ -56,8 +57,10 @@ welcome();
 
 // Multiline arrow functions
 
-// Sometimes we need a more complex function, with multiple expressions and statements. In that case, we can 
-// enclose them in curly braces. The major difference is that curly braces require a return within them to return 
+// Sometimes we need a more complex function, with multiple expressions and statements. 
+// In that case, we can 
+// enclose them in curly braces. The major difference is that curly braces require a 
+// return within them to return 
 // a value (just like a regular function does).
 
 // Like this:
